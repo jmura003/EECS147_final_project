508ed4c9938f88971a36cc00a037774548ad5eff
Marvin Dela Cruz <mdela022@ucr.edu>
2019-06-12 15:58:47 -0700

    Added Pycuda implementation (Wrong indexing)

---
+++ pycuda/pycuda_tri_rast.py
@@ -0,0 +1,105 @@
+import array
+import numpy
+import sys
+import math
+import random
+import time
+import pycuda.autoinit
+import pycuda.driver as cuda
+import pycuda.gpuarray as gpuarray
+from pycuda.compiler import SourceModule
+from PIL import Image
+
+mod = SourceModule("""
+__device__ inline
+float edgeFunction(const float a[], const float b[], const float c[])
+{ return (c[0] - a[0]) * (b[1] - a[1]) - (c[1] - a[1]) * (b[0] - a[0]); }
+
+__global__ void rasterizePixel(float* triangles, float *image, int size)
+{
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    int j = ty + blockIdx.y * blockDim.y; // rows
+    int i = tx + blockIdx.x * blockDim.x; // cols
+
+    float p[2] = {i + 0.5f, j + 0.5f};
+    for(int k = 0; k < size*6; k+=6)
+    {
+      float v0[2] = {triangles[k], triangles[k + 1]};
+      float v1[2] = {triangles[k + 2], triangles[k + 3]};
+      float v2[2] = {triangles[k + 4], triangles[k + 5]};
+      float area = edgeFunction(v0, v1, v2);
+      int index = (i + j * 512) * 3;
+      float alpha = edgeFunction(v1, v2, p);
+      float beta = edgeFunction(v2, v0, p);
+      float gamma = edgeFunction(v0, v1, p);
+
+      __syncthreads();
+
+      if(alpha >= 0 && beta >= 0 && gamma >= 0){
+        alpha = alpha / area;
+        beta = beta / area;
+        gamma = gamma / area;
+        float r = alpha;
+        float g = beta;
+        float b = gamma;
+        if(i < 512 && j < 512)
+        {
+          image[index] = (r * 255);
+          image[index + 1] = (g * 255);
+          image[index + 2] = (b * 255);
+        }
+      }
+
+      __syncthreads();
+    }
+}
+""")
+
+w = 512
+h = 512
+maxval = 255
+
+triangles = []
+
+# PPM file header
+ppm_header = 'P6 ' + str(w) + ' ' + str(h) + ' ' + str(maxval) + '\n'
+
+def makeTriangles(n, w, h): #Does not prevent creating duplicate triangles
+	#n determines number of triangles within a w x h image to generate in an array
+
+    global triangles
+    for i in range(0, n):
+        for j in range(0, 3):
+            x = random.uniform(0, w)
+            y = random.uniform(0, h)
+            triangles.append(x)
+            triangles.append(y)
+    npTriangles = numpy.array(triangles, dtype=numpy.float32)
+    return npTriangles
+
+# PPM image data (filled with black)
+image = numpy.zeros(3*w*h).astype(numpy.float32)
+
+sync = pycuda.driver.Stream()
+func = mod.get_function("rasterizePixel")
+gridParam = int(math.ceil(512/32))
+
+start = time.time()
+
+sizeInput = input("Enter the number of triangles: ")
+trianglesParam = makeTriangles(sizeInput, 512, 512)
+size = numpy.int32(sizeInput)
+func(cuda.InOut(trianglesParam), cuda.InOut(image), size, grid=(gridParam, gridParam, 1), block=(32,32,1))
+sync.synchronize();
+
+end = time.time()
+
+# Save the PPM image as a binary file
+with open('temp.ppm', 'wb') as f:
+    f.write(bytearray(ppm_header, 'ascii'))
+    image.tofile(f)
+    im = Image.open("temp.ppm")
+    im.save("testpic.png")
+
+print("Time elapsed for " + str(sizeInput) + " triangles is " + str(end - start))
